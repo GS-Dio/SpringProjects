57583e425c70fc350cf9bcada9383f353c22cac3
GS-Dio <149898417+GS-Dio@users.noreply.github.com>
2024-04-24 23:44:59 +0500

    Delete Employment-Management-System directory

--- Employment-Management-System/src/main/java/kz/dio/Employment/Management/System/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package kz.dio.Employment.Management.System.repository;
-
-import kz.dio.Employment.Management.System.domain.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
